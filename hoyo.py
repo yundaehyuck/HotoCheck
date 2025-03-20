2eebb6892008a8590edce574569ddb6889a340c4
yundaehyuck <dbseogur2000@naver.com>
2025-03-21 00:24:16 +0900

    loading bug fix + headless mode

--- hoyo.py
+++ hoyo.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # 웹 드라이버 실행 (Chrome 기준)
 
@@ -12,23 +13,15 @@ chrome_options.add_argument("--headless")  # GUI 없이 실행
 driver = webdriver.Chrome()
 driver.get("https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&hyl_auth_required=true&hyl_presentation_style=fullscreen&utm_source=hoyolab&utm_medium=tools&lang=ko-kr&bbs_theme=light&bbs_theme_device=1")  # 원하는 웹사이트 URL
 
-time.sleep(2)
+wait = WebDriverWait(driver, 10)  # 최대 10초 대기
 
-# XPath를 사용하여 버튼 요소 찾기
-button = driver.find_element(By.XPATH, "//span[contains(@class, 'guide-close')]")
-
-# 버튼 클릭
+# 닫기 버튼 클릭
+button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'guide-close')]")))
 button.click()
 
-time.sleep(1)
-
-# XPath를 사용하여 버튼 요소 찾기
-button = driver.find_element(By.XPATH, "//img[contains(@class, 'mhy-hoyolab-account-block__avatar-icon')]")
-
-# 버튼 클릭
+# 로그인 창 불러오기
+button = wait.until(EC.element_to_be_clickable((By.XPATH, "//img[contains(@class, 'mhy-hoyolab-account-block__avatar-icon')]")))
 button.click()
-
-time.sleep(1)
 
 # 2. 입력 필드 찾기 (XPath 사용)
         
@@ -36,54 +29,43 @@ time.sleep(1)
 #iframe = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[1]")
 driver.switch_to.frame(0)
 
-id = driver.find_element(By.XPATH, "//input[@name='username' and @type='text']")
-
-# 3. 클릭
-id.click()
+id = wait.until(
+    EC.presence_of_element_located((By.XPATH, "//input[@name='username' and @type='text']"))
+)
+#id.click()
 
 # # 4. 아이디 입력
 id.send_keys("your id")  # 여기에 아이디 입력
-time.sleep(1)
 
-password = driver.find_element(By.XPATH, "//input[@name='password' and @type='password']")
+# 비밀번호 입력
+password_input = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@name='password' and @type='password']")))
+password_input.send_keys("your password")
 
-# 3. 클릭
-password.click()
-
-# 4. 아이디 입력
-password.send_keys("your password")  # 여기에 아이디 입력
-time.sleep(1)
-
-login = driver.find_element(By.XPATH,"//button[@type='submit' and contains(@class, 'el-button--primary')]/span[text()='로그인']")
-
-login.click()
-
-time.sleep(2)
+# 로그인 버튼 클릭
+login_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit' and contains(@class, 'el-button--primary')]/span[text()='로그인']")))
+login_button.click()
 
 # 로그인 후, 기본(원래) 프레임으로 돌아가기
 driver.switch_to.default_content()
 
-button = driver.find_element(By.XPATH,"//span[text()='더보기']")
+# "더보기" 버튼 클릭
+more_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='더보기']")))
+more_button.click()
 
-button.click()
-time.sleep(2)
-
-count = driver.find_element(By.XPATH,"//span[@class='sign-num']").text
-
-count = int(count)
+# 현재 출석일수 가져오기
+count_element = wait.until(EC.presence_of_element_located((By.XPATH, "//span[@class='sign-num']")))
+count = int(count_element.text)
 
 target = count + 1
 
 xpath = f"//div[contains(@class, 'components-home-assets-__sign-content-test_---item-day---1C_BmH') and contains(text(), '{target}일째')]"
 
-button = driver.find_element(By.XPATH,xpath)
-button.click()
+sign_button = wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+sign_button.click()
 
-time.sleep(2)
+print('출석체크 완료')
 
 driver.quit()
-
-time.sleep(1)
 
 ###########################################################
 
